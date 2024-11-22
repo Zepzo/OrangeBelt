718bc1fc77efb0fafd411e9a28edde3084d360d5
Zätha Malmberg <zathamalmberg@gmail.com>
2024-11-22 14:01:38 +0100

    Lamda exercices complete

---
+++ LamdaExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LamdaExercise;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var caracter = new Character();
+        var random = new Random();
+
+        int attackChance = 5;
+
+        List<string> enemies = new() { "enemy", "enemy", "enemy" };
+        
+        caracter.SpecialAttack(enemies, () => attackChance < random.Next(0,11));
+        
+    }
+}
+
+class Character
+{
+    public void SpecialAttack(List<string> enemies, Func<bool> canAttack)
+    {
+        foreach (var enemyList in enemies)
+        {
+            if (canAttack())
+            {
+                Console.WriteLine($"Special attack on {enemyList}");
+            }
+        }
+    }
+}
